efd69ad1539f3b967cd7e1e098c6e62873d2cead
Ethan Latimer <ethanlatimer@hotmail.co.uk>
2024-03-31 16:45:43 +0100

    Changed car updates to a separate GUI with tkinter

--- car-ecu.py
+++ car-ecu.py
@@ -2,39 +2,72 @@
 
 import threading
 import time
-import matplotlib.pyplot as plt
-import numpy as np
 import math
+import curses
+import tkinter as tk
 
-plt.style.use('_mpl-gallery')
 
 """
 Module Docstring
 """
 ENGINE_REDLINE = 15,000
 
-ECU_CAR_CLOCK = time.perf_counter()
-ECU_ENGINE_SPEED = 3500 
-ECU_CAR_SPEED = 0
-ECU_CAR_GEAR = 8
-ECU_CAR_THROTTLE = 1
-ECU_CAR_BRAKE = 0
+root = tk.Tk()
 
-x = []
-y = []
+ECU_CLOCK = time.perf_counter()
+
+ECU_CAR_CLOCK = tk.IntVar(root, ECU_CLOCK)
+ECU_ENGINE_SPEED = tk.DoubleVar(root, 3500.00)
+ECU_CAR_SPEED = tk.DoubleVar(root, 0.00)
+ECU_CAR_GEAR = tk.IntVar(root, 8)
+ECU_CAR_THROTTLE = tk.DoubleVar(root, 1.00)
+ECU_CAR_BRAKE = tk.DoubleVar(root, 0.00)
+
+current_command = ""
+trigger_input = False
 
 __author__ = "Ethan"
 __license__ = "MIT"
 
-def updateECU():
-    while True:
-        print ("Clock: " , ECU_CAR_CLOCK)
-        print ("Engine Speed: " , ECU_ENGINE_SPEED)
-        print ("Car Speed: " , ECU_CAR_SPEED)
-        print ("Gear: " , ECU_CAR_GEAR)
-        print ("Throttle: " , ECU_CAR_THROTTLE)
-        print ("Brake: " , ECU_CAR_BRAKE)
-        time.sleep(0.03)
+##
+## Handle Commands
+##
+
+def cursesCommandLine(stdscr):
+    stdscr.clear()
+
+    stdscr.addstr(0, 0, "This is the car simulation command line")
+    stdscr.addstr(1, 0, "Enter commands to change the behaviour of the car, type HELP for the list of commands")
+    stdscr.refresh()
+
+    while (current_command == "EXIT"):
+        
+
+    exit()
+
+    
+
+
+
+# ** Commands List ** 
+# THROTTLE <INT>
+# BRAKE <INT>
+# POWER <Bool>
+def handle_commands():
+    global current_command
+    if current_command == "":
+        return
+
+    tokens = current_command.split(" ")
+    match tokens[0]:
+        case "THROTTLE":
+            if(tokens[1] > 0 and tokens[1] <= 100):
+                ECU_CAR_THROTTLE.set(tokens[1] / 100)
+
+
+##
+## Car Simulation
+##
 
 def simulateCar():
     while True:
@@ -42,27 +75,26 @@ def simulateCar():
         global ECU_ENGINE_SPEED
         global ECU_CAR_GEAR
         global ECU_CAR_SPEED
-        
-        oldTime = ECU_CAR_CLOCK 
-        ECU_CAR_CLOCK = time.perf_counter()
+        global ECU_CLOCK
 
-        timeDiff = ECU_CAR_CLOCK - oldTime
-        if (ECU_ENGINE_SPEED > 15000):
-            break;
+        # handle_commands()
+        oldTime = ECU_CLOCK
+        ECU_CLOCK = time.perf_counter()
+
+        timeDiff = ECU_CLOCK - oldTime
+
+        ECU_CAR_CLOCK.set(ECU_CLOCK)
 
 
-        ECU_ENGINE_SPEED = engine_speed_model(ECU_ENGINE_SPEED, ECU_CAR_GEAR, 1, timeDiff)
-        ECU_CAR_SPEED = getSpeedOfCar(ECU_CAR_GEAR, ECU_ENGINE_SPEED)
-        
-        x.append(ECU_CAR_CLOCK)
-        y.append(ECU_ENGINE_SPEED)
+        if (ECU_ENGINE_SPEED.get() < 15000):
+            ECU_ENGINE_SPEED.set( round( engine_speed_model(ECU_ENGINE_SPEED.get(), ECU_CAR_GEAR.get(), 1, timeDiff), 1))
+            ECU_CAR_SPEED.set( round( getSpeedOfCar(ECU_CAR_GEAR.get(), ECU_ENGINE_SPEED.get()) , 1) )
 
-        time.sleep(0.03)
+        time.sleep( 1 / 15 )
 
 # Returns spedd in km/h
 def getSpeedOfCar(currentGear, engineRPM):
     constant = 110 + ((currentGear - 1) * 35)
-
     return constant * (engineRPM / 15000)
 
 
@@ -95,27 +127,60 @@ def engine_speed_model(currentSpeed, gear, throttle, timeelapsed):
 
     return (constant * newTimeAcc * throttle) ** 2
 
-def main():
+
+##
+## Handle GUI Components
+##
+
+def buildGUI():
+    root.title("Live data")
+    root.geometry("400x150")
+
+    frame = tk.Frame(root)
+
+    # All labels
+    clock_header_label = tk.Label(frame, text="CLOCK: ")
+    enginespeed_header_label = tk.Label(frame, text="ENGINE SPEED: ")
+    carspeed_header_label = tk.Label(frame, text="CAR_SPEED: ")
+    gear_header_label = tk.Label(frame, text="CAR_GEAR: ")
+    throttle_header_label = tk.Label(frame, text="CAR_THROTTLE_PERCENTAGE: ")
+    brake_header_label = tk.Label(frame, text="CAR_BRAKE_PERCENTAGE")
+
+    clock_header_label.grid(row=0, column=0) 
+    enginespeed_header_label.grid(row=1, column=0)
+    carspeed_header_label.grid(row=2, column=0)
+    gear_header_label.grid(row=3, column=0)
+    throttle_header_label.grid(row=4, column=0)
+
+    enginespeed_label   = tk.Label(frame, textvariable = ECU_ENGINE_SPEED)
+    carspeed_label      = tk.Label(frame, textvariable = ECU_CAR_SPEED)
+    clock_label         = tk.Label(frame, textvariable = ECU_CAR_CLOCK)
+    gear_label          = tk.Label(frame, textvariable = ECU_CAR_GEAR)
+    throttle_label      = tk.Label(frame, textvariable = ECU_CAR_THROTTLE)
+        
+    clock_label.grid(row=0, column=1) 
+    enginespeed_label.grid(row=1, column=1)
+    carspeed_label.grid(row=2, column=1)
+    gear_label.grid(row=3, column=1)
+    throttle_label.grid(row=4, column=1)
+
+    frame.pack()
+    root.mainloop()
+
+def main(s):
     """ Main entry point of the app """
-    print_thread = threading.Thread(target=updateECU)
-    print_thread_daemon = True
 
     carsim_thread = threading.Thread(target=simulateCar)
-    print_thread_daemon = True
+    carsim_thread_daemon = True
+
+    curses_thread = threading.Thread(target=cursesCommandLine)
+    curses_thread_daemon = True
 
     carsim_thread.start()
+    curses_thread.start()
 
-    print_thread.start()
-
-    time.sleep (5)
-    fig, ax = plt.subplots()
-    ax.plot(x, y, linewidth=2.0)
-    plt.show()
-
-
+    buildGUI()
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
-
+    wrapper(main)
 
