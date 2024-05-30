d44ee982e0e290e560f4497ad83a341322c2d607
Ethan Latimer <ethanlatimer@hotmail.co.uk>
2024-05-30 23:42:00 +0100

    Initialised build system with debugging

---
+++ pico/src/blink.c
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+int main() {
+    stdio_init_all();
+	while (1) {
+		printf("Hello world");
+		sleep_ms(300);
+	}
+}
